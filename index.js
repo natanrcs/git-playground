008e8214394f51d98df6d55f4d8f873f87d2e396
natanrcs <nathanrcs2022@gmail.com>
2026-06-11 10:30:09 -0300

    feat- refatorando erros no javascript puro suco

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const inputName = document.getElementById("inputName").value;
-const inputEmail = document.getElementById("inputEmail").value;
-const messageContent = document.getElementById("messageContent").value;
+let databaseTempory = [];
 const button = document.getElementById("button");
-databaseTempory = []
 
-function functiongetDados(databaseTempory,inputName,inputEmail,messageContent){
-    allDados = {"names": inputName,"emails": inputEmail,"messages":messageContent};
-    if(allDados.names === "" | allDados.emails === "" | allDados.messageContent === ""){
-        alert("Preencha os campos abaixos!");
-    };
-    databaseTempory.append(allDados);
-    console.log("Dados salvos no database.");
+function functiongetDados(){
+    const inputName = document.getElementById("inputName").value;
+    const inputEmail = document.getElementById("inputEmail").value;
+    const messages = document.getElementById("messageContent").value;
+
+    const allDados = {"names":inputName,"emails":inputEmail,"messages":messages};
+    if(allDados.names === "" || allDados.emails === "" || allDados.messages === ""){
+        alert("Preencha os campos!");
+        return;
+    }
+    databaseTempory.pop(allDados)
+    console.log("Salvos com sucesso no database temporario",databaseTempory);
 };
 button.addEventListener("click",functiongetDados);
