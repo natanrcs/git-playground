f68b708175465c974e6e7e2852e8d51d907c3e85
Natan Santos <nathanrcs2022@gmail.com>
2026-07-27 09:31:50 -0300

    deixando certo a coisa

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-const button = document.getElementById("theme-toggle");
-button.addEventListener("click", ()=>{
+const buttonTheme = document.getElementById("theme-toggle");
+buttonTheme.addEventListener("click",()=>{
    document.body.classList.toggle("dark-mode");
+   console.log("tema alterado com sucesso!")
 });
-=======
-// Seleciona apenas o botão pelo ID
-const botao = document.getElementById("theme-toggle");
-
-// Adiciona o evento de clique
-botao.addEventListener("click", () => {
-   // O 'toggle' adiciona 'dark-mode' se não existir, ou remove se já existir
-   document.body.classList.toggle("dark-mode");
-});
->>>>>>> 1a2cf675d852f67bf8072f9f106aa7e16a7d965c
