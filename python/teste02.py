fd56102556b29d391ba2588bcfc2ae13f243605e
Natan Santos <nathanrcs2022@gmail.com>
2026-07-29 12:03:35 -0300

    aprendendo sempre

---
+++ python/teste02.py
@@ -0,0 +1,15 @@
+#tipos de dados
+
+
+texto = "Python" #string
+numero = 10 #inteiro
+decimal = 10.5 #decimal
+ativo = True #boolean
+
+def tipos():
+    for i in range(1):
+        print(type(texto))
+        print(type(numero))
+        print(type(decimal))
+        print(type(ativo))
+tipos()
