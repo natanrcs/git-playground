073f7023370ab38109a6a40999173fb902da1389
Natan Santos <nathanrcs2022@gmail.com>
2026-08-03 12:27:55 -0300

    arrumando campo de password para str

--- python/exemple03.py
+++ python/exemple03.py
@@ -1,9 +1,9 @@
 user_admin = {
-    "name": "natan","password": 3591
+    "name": "natan","password":"3591"
 }
 def login():
     user = input("digite seu nome: ")
-    password = int(input("digite sua senha: "))
+    password = input("digite sua senha: ")
     if user == user_admin.get("name") and password == user_admin.get("password"):
         print("acesso liberado!")
     else:
