48fc18526a5d0329ebfdc01571cfac3ac839aace
Natan Santos <nathanrcs2022@gmail.com>
2026-08-03 12:47:38 -0300

    refatorando script em pythonista

--- python/exemple03.py
+++ python/exemple03.py
@@ -1,11 +1,19 @@
-user_admin = {
-    "name": "natan","password":"3591"
-}
-def login():
-    user = input("digite seu nome: ")
-    password = input("digite sua senha: ")
-    if user == user_admin.get("name") and password == user_admin.get("password"):
-        print("acesso liberado!")
-    else:
-        print("acesso negado!")
-login()
+DATABASE = []
+user_admin =[
+    {"name":"natan","password": "3591"},
+    {"name":"gabi","password":"2501"},
+    {"name":"melinda","password":"2505"},
+    {"name":"will","password":"8890"},
+    {"name":"dogao","password":"2567"}
+]
+
+def acess_login():
+    user_name = input("digite seu nome: ")
+    user_password = input("digite sua senha: ")
+    for i in user_admin:
+        if user_name == i.get("name") and user_password == i.get("password"):
+            DATABASE.append(user_name)
+            print("acesso liberado!")
+            return
+    print("acesso negado!")
+acess_login()
